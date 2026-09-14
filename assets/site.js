62c2505da17bc89679849196571169fe230ec6ee
aalsahli5512-lang <aalsahli5512@gmail.com>
2026-09-14 05:04:26 +0300

    Add lightweight navigation script

---
+++ assets/site.js
@@ -0,0 +1,1 @@
+const menuButton=document.querySelector('.menu');const links=document.querySelector('.links');if(menuButton&&links){menuButton.addEventListener('click',()=>links.classList.toggle('open'));links.querySelectorAll('a').forEach(a=>a.addEventListener('click',()=>links.classList.remove('open')))}document.getElementById('year')?.append(new Date().getFullYear());
